949e7800307d0cdeae1ce8c54dcb6afcfc92b577
visot <visotskiy.alexey@gmail.com>
2023-04-16 22:00:28 +0700

    Задание 16.4.1

---
+++ Task16_4_1/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task16_4_1
+{
+    public class Calculator
+    {
+        public int Addition(int a, int b, int c) => a + b + c;
+
+        public int Multiplication(int a, int b, int c) => a*b*c;
+    }
+}
---
+++ Task16_4_3.Tests/CalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using Task16_4_1;
+using Xunit;
+
+namespace Task16_4_3.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Addition(50, 10, 34);
+            Assert.Equal(94, result);
+        }
+
+        [Fact]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Multiplication(3, 2, 20);
+            Assert.Equal(120, result);
+        }
+    }
+}
