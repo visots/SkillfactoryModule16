a7e781780473720e61847a195faf48f453b1a06b
visot <visotskiy.alexey@gmail.com>
2023-04-19 19:53:21 +0700

    Итоговое задание 16.7.1

---
+++ FinalTask16_7_1.Tests/CalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace FinalTask16_7_1.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Additional(100,100);
+            Assert.That(result == 200);
+        }
+
+        [Test]
+        public void SubstractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Subtraction(100, 100);
+            Assert.True(result == 0);
+        }
+
+        [Test]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Multiplication(5, 5);
+            Assert.That(result == 25);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Division(25, 5);
+            Assert.That(result == 5);
+        }
+    }
+}
